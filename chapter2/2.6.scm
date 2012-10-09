d7259d55e46836ffe6b41f3bd50fc65c2b0572f0
Ryota Sugimoto <ryota.sugimoto@gmail.com>
2012-10-09 16:00:05 +0900

    added comment

--- chapter2/2.6.scm
+++ chapter2/2.6.scm
@@ -9,6 +9,7 @@
 
 (define three (lambda (f) (lambda (x) (f (f (f x))))))
 
+;I used "plus" instead of "+" because "+" will collide with covert function.
 (define (plus a b)
   (lambda (f) (lambda (x) ((b f) ((a f) x)))))
 
@@ -16,6 +17,7 @@
 ;convert church numeral to integer
 (define (convert church)
   ((church (lambda (n) (+ n 1))) 0))
+
 (newline)
 (display (convert zero))
 (newline)
